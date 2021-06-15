e35713072e318ce649559c6084f297fcf97baf6d
Fernando Nakano <fernakano@gmail.com>
2021-06-15 14:33:25 -0400

    adding some comments to explain functionality

--- Assignment1/eliza.py
+++ Assignment1/eliza.py
@@ -118,26 +118,23 @@ RESPONSE_CONVERTERS = {
 }
 
 
-def is_valid(input_text):
-    if input_text.strip(' ') == '':
-        return True
-    # check for only numbers and symbols:
-    word_tokens = word_tokenize(input_text)
-    for word in word_tokens:
-        if word.isalpha():
-            return True
-    else:
-        return False
-
-
 def main():
+    """This is the Main execution function for Eliza
+
+    This is where the execution starts, it will work to process the iterated loop for Eliza Chat Bot.
+    The idea is that here we will have an Infinite Loop that is stopped by specific keywords like:
+     'exit, quit, bye or goodbye'
+    on each iteration of the loop the program will wait for an input from the User with the response process
+    from the previous message.
+    :return: None
+    """
     print(
-          '******************************************************************\n'
-          '*                                                                *\n'
-          '*    Welcome to your therapist--to end, simply type "exit"...    *\n'
-          '*                                                                *\n'
-          '******************************************************************\n'
-          )
+        '******************************************************************\n'
+        '*                                                                *\n'
+        '*    Welcome to your therapist--to end, simply type "exit"...    *\n'
+        '*                                                                *\n'
+        '******************************************************************\n'
+    )
 
     message = CONVERSATION_STARTER
     while True:
@@ -153,6 +150,24 @@ def main():
 
 
 def process(user_input):
+    """Bot message processor
+
+    This function will receive the user_input and will try to understand the input based on the RULES that were defined
+    on the RULES Dictionary at the top of the application.
+
+    - First it will preprocess by tokenizing and normalizing the input (remove punctuation, special characters) and
+    lower the text input
+    - With the normalized_user_input, then it will for each RULE in the RULES Dictionary try to match the regex
+    on the RULE with the normalized_user_input and if there is a match will look for the last text on the regex group match
+    and replace/format in the text with the placeholder {}.
+    - Special Case:
+        When the Rule is of Type 'name' we will store the information for later use.
+        for all responses found in the RULES match output, we will try to replace the placeholder {{NAME}}
+        with the value of USER_NAME global variable to represent person identification from the Bot.
+
+    :param user_input:
+    :return: sentence
+    """
     global USER_NAME
     normalized_user_input = " ".join(normalize_and_tokenize(user_input))
     for regex, rule in RULES.items():
@@ -168,27 +183,77 @@ def process(user_input):
             return sentence
 
 
+def is_valid(input_text):
+    """Verify if input is valid to proceed to RULE parsing.
+        if the input is valid to proceed this method will return True otherwise False.
+
+        if there is a single special character as a text this input is not valid.
+
+    :param input_text:
+    :return: Boolean
+    """
+    if input_text.strip(' ') == '':
+        return True
+    # check for only numbers and symbols:
+    word_tokens = word_tokenize(input_text)
+    for word in word_tokens:
+        if word.isalpha():
+            return True
+    else:
+        return False
+
+
 def normalize_and_tokenize(text):
+    """
+    This function will Lower the text and call the word_tokenizer(text) to retrieve the tokenized lowered text
+    :param text:
+    :return: lowered tokenized text.
+    """
     tokens = word_tokenize(text.lower())
     return tokens
 
 
 # for this scenario, maybe string split() could possibly be enough.
 def word_tokenize(text):
+    """Work Tokenizer
+        This Tokenizer will user a RegexpTokenizer \w+
+        to identify words from the sentences, this will exclude punctuations and special characters while tokenizing.
+
+    :param text:
+    :return: tokenized text
+    """
     tokenizer = RegexpTokenizer(r'\w+')
     return tokenizer.tokenize(text)
 
 
 def convert_response_as_text(tokens):
+    """ Reponse Grammar perspective converter
+    This will return a joined sentence from tokens while at the same time changing the response grammar perspective.
+    :param tokens:
+    :return:
+    """
     return " ".join(change_perspective(tokens))
 
 
 def change_perspective(tokens):
+    """ Change Text grammar point of view from user to Eliza BOT.
+    This function change perspective will use the dictionary RESPONSE_CONVERTERS and iterate through each item to
+    replace texts from the Key to the value in order to change the user's perspective accordingly to the mapping provided.
+
+    we ignore case for this comparison to make regex easier and more concise to write on the mapping.
+
+    For example, if the user says: "i am crazy" the output should be "you are crazy"
+
+    :param tokens:
+    :return: grammar perspective user change.
+    """
     for i, j in enumerate(tokens):
         for convert_from, convert_to in RESPONSE_CONVERTERS.items():
             tokens[i] = re.sub(convert_from, convert_to, tokens[i], flags=re.IGNORECASE)
     return tokens
 
 
+# This indicates that the program is being executed from the command line
+# and that the initial function to be called is main()
 if __name__ == "__main__":
     main()

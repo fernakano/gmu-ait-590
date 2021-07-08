42fae555c1e398e9bf60a6fd09b6c53a11edd32a
melissa.cirtain <melissa.cirtain@u.group>
2021-07-07 22:52:32 -0400

    have some rudimentary searching. next: match summary and form answer

--- Assignment3/qa-system.py
+++ Assignment3/qa-system.py
@@ -11,14 +11,26 @@
 """
 Description:
 
-BONUS Functionality:
+BONUS Functionality:  Use SpaCy's NER to perform advanced
+entity recognition to assist with improved question understanding
+and accuracy of answers!
 
 Example Output:
 
 """
+
+# for teting: try running with these questions:
+# who is the National Park Service and is it part of the USA? Is Melissa available?
+# what is the date?
+
 import sys
 import wikipedia
 import re
+import spacy
+import en_core_web_sm
+
+# load English tokenizer, tagger, parser, NER, and word vectors
+nlp = en_core_web_sm.load() # load here, takes a sec
 
 
 def make_introduction():
@@ -34,18 +46,140 @@ def make_introduction():
 
 def qstn_is_valid(qstn):
     '''Return True if qstn begins with who, what, when, or where'''
-    if re.match(r"^([W|w]hen|[W|w]here|[W|w]ho|[W|w]hat)\b", qstn):
+    if re.match(r"^([W|w]hen|[W|w]here|[W|w]ho|[W|w]hat)\b", qstn.lower()):
         return True
     else:
-        print('baa--wrong start!')
         return False
+
+
+def get_nes(qstn):
+    '''return a list of Named Entities or an empty list if none found'''
+    
+    # named entities list
+    nes = []
+
+    # use SpaCy to identify NEs
+    mytext = nlp(qstn)
+
+    for entity in mytext.ents:
+        nes.append(entity)
+        print(f'entity: {entity}, ({entity.label_})')
+
+    if nes == []:
+        print(f'no named entities found for {qstn}')
+
+    return nes
+
+
+def query_wiki(qstn, nes):
+    '''query our data sources online, return text result'''
+
+    long_answer = ''
+    summaries = []
+
+    # TODO: implement wikipedia/google queries
+    for ne in nes:
+        print(f'querying on ne {ne.text}')
+        titles = wikipedia.search(ne.text)
+        for t in titles:
+            try:
+                page = wikipedia.page(t)
+                summaries.append(wikipedia.summary(t, sentences=1))
+            except Exception as e:
+                pass
+
+        print(f'Summaries:')
+        for s in summaries:
+            print(s[0:50])
+
+    return summaries
+
+
+def answer_who(qstn, nes, long_answer):
+    ''' handle questions beginning with "who" '''
+
+    # TODO: formulate a number of "who" answers around the NE result, XYZ
+    # 1. who is XYZ: XYZ is|was ABC
+    # 2. who <VERB> XYZ: ABC <VERBED> XYZ
+    # 3. who is the XYZ: ABX is the XYZ
+
+    answer = 'ABC answer'
+    return answer
+
+
+def answer_what(qstn, nes, long_answer):
+    ''' handle questions beginning with "what" '''
+
+    # TODO: formulate a number of "what" questions/answers around the NEs
+    # 1. what is the XYZ: XYZ is ABC
+    # ...
+
+    answer = 'ABC answer'
+    return answer
+
+
+def answer_when(qstn, nes, long_answer):
+    ''' handle questions beginning with "when" '''
+
+    # TODO: formulate a number of "when" questions/answers around the NEs
+    # 1. when is XYZ: XYZ is at|on|after|during|in ABC
+    # ...
+
+    answer = 'ABC answer'
+    return answer
+
+
+def answer_where(qstn, nes, long_answer):
+    ''' handle questions beginning with "where" '''
+
+    # TODO: formulate a number of "where" questions/answers around the NEs
+    # 1. where is the XYZ: XYZ is in|at|on (the)+ ABC
+    # ...
+
+    answer = 'ABC answer'
+    return answer
 
 
 def get_answer(qstn):
     '''use online resources to try to answer question.'''
 
+    # get NERs from spacy
+    nes = get_nes(qstn)
+    
+    # query for data
+    long_answer = query_wiki(qstn, nes)
+
     # look up an answer to the question:
     # TODO: lookup an answer, return 'some answer for now'
+
+    # get the first word
+    w_word = re.match(r"^([\w\-]+)", qstn.lower())
+    
+    # answer depending on first word (who/what/where/when)
+    if w_word.group().lower() == 'who':
+        # TODO: handle 'who' type questions
+        print('WHO!')
+        ans = answer_who(qstn, nes, long_answer)
+
+    elif w_word.group().lower() == 'what':
+        # TODO: handle 'what' type questions
+        print('WHAT!')
+        ans = answer_what(qstn, nes, long_answer)
+
+    elif w_word.group().lower() == 'where':
+        # TODO: handle 'where' type questions
+        ans = answer_where(qstn, nes, long_answer)
+        print('WHERE!')
+
+    elif w_word.group().lower() == 'when':
+        # TODO: handle 'when' type questions
+        ans = answer_when(qstn, nes, long_answer)
+        print('WHEN!')
+
+    else:
+        print('ERROR--should not get here...')
+        assert False    
+
     return 'some answer for now'
 
 

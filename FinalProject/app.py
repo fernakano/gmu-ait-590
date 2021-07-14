7786747ccc2eb130c5b8339b33355041aaa93ac8
Fernando Nakano <fernakano@gmail.com>
2021-07-13 22:49:50 -0400

    adding final report update

--- FinalProject/app.py
+++ FinalProject/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, Markup
 import json
 import uuid
 import job_prescreener as ps
@@ -18,15 +18,20 @@ def application():
         'token': str(uuid.uuid4()),
         'timestamp': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
         'name': str(" ".join([request.form['first_name'], request.form['last_name']])),
-        'profile': request.form['job_profile'],
-        'behavioral_questions': [request.form['job_conflict']]
+        'job_profile': request.form['job_profile'],
+        'job_matches': [],
+        'behavioral_questions': [request.form['job_conflict']],
+        'behavioral_sentiments': []
     }
 
     print("Applicant: ", str(candidate['name']))
-    print("Job Profile: ", str(candidate['profile']))
+    print("Job Profile: ", str(candidate['job_profile']))
     print("Job Conflict: ", str(candidate['behavioral_questions'][0]))
 
     candidate = ps.candidate_evaluation(candidate)
+
+    # test sample
+    candidate['job_matches'] = ['sample job 1', 'sample job 2']
 
     return redirect('/report?' +
                     "&".join([
@@ -37,7 +42,8 @@ def application():
 
 @app.route("/report")
 def report():
-    return render_template('report.html')
+    candidate = json.loads(Markup(request.args.get("candidate")))
+    return render_template('report.html', candidate=candidate)
 
 
 app.run(debug=True)
